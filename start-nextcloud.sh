ae6febf6f57b5286761d93bfee86660ead513651
ln-komandur <68081200+ln-komandur@users.noreply.github.com>
2021-07-27 17:27:38 -0500

    Stop & start 4 services, manage partition mounting

---
+++ start-nextcloud.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#ref: https://unix.stackexchange.com/questions/28791/prompt-for-sudo-password-and-programmatically-elevate-privilege-in-bash-script
+#ref: https://askubuntu.com/a/30157/8698
+
+if (($EUID != 0)); then
+  if [[ -t 1 ]]; then
+#https://unix.stackexchange.com/questions/218715/what-does-t-1-do
+    sudo "$0" "$@"
+  else
+    exec 1>output_file
+    gksu "$0 $@"
+  fi
+  exit
+fi
+echo "This script reports the mount status of the NextCloud Data partition and mounts it too. After"
+echo "mounting it, stops nextcloud services namely PHP Session Clean timer, PHP7.4fpm, MariaDB, Apache2."
+echo "AUTHENTICATION SUCCESSFUL. You are executing the script as" $USER
+echo
+echo
+echo "---------------------------------------------------------------------------------------------------"
+echo "Mount status of /dev/sda6"
+echo "---------------------------------------------------------------------------------------------------"
+findmnt /dev/sda6
+echo
+echo
+echo "---------------------------------------------------------------------------------------------------"
+echo "Mounting /dev/sda6"
+echo "---------------------------------------------------------------------------------------------------"
+mount /dev/sda6
+echo
+echo
+echo "---------------------------------------------------------------------------------------------------"
+echo "Status of phpsessionclean.timer, php7.4-fpm.service, mariadb.service, apache2.service"
+echo "---------------------------------------------------------------------------------------------------"
+systemctl status phpsessionclean.timer php7.4-fpm.service mariadb.service apache2.service
+echo
+echo
+echo "---------------------------------------------------------------------------------------------------"
+echo "Starting phpsessionclean.timer, php7.4-fpm.service, mariadb.service, apache2.service"
+echo "---------------------------------------------------------------------------------------------------"
+systemctl start phpsessionclean.timer php7.4-fpm.service mariadb.service apache2.service
+echo
+echo
+echo "Exit"
---
+++ stop-nextcloud.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#ref: https://unix.stackexchange.com/questions/28791/prompt-for-sudo-password-and-programmatically-elevate-privilege-in-bash-script
+#ref: https://askubuntu.com/a/30157/8698
+
+if (($EUID != 0)); then
+  if [[ -t 1 ]]; then
+#https://unix.stackexchange.com/questions/218715/what-does-t-1-do
+    sudo "$0" "$@"
+  else
+    exec 1>output_file
+    gksu "$0 $@"
+  fi
+  exit
+fi
+echo "This script stops nextcloud services namely PHP Session Clean timer, PHP7.4fpm, MariaDB, Apache2. After"
+echo "stopping these services, it reports the mount status of the NextCloud Data partition and unmounts it too"
+echo "AUTHENTICATION SUCCESSFUL. You are executing the script as" $USER
+echo
+echo
+echo "---------------------------------------------------------------------------------------------------"
+echo "Status of phpsessionclean.timer, php7.4-fpm.service, mariadb.service, apache2.service"
+echo "---------------------------------------------------------------------------------------------------"
+systemctl status phpsessionclean.timer php7.4-fpm.service mariadb.service apache2.service
+echo
+echo
+echo "---------------------------------------------------------------------------------------------------"
+echo "Stopping phpsessionclean.timer, php7.4-fpm.service, mariadb.service, apache2.service"
+echo "---------------------------------------------------------------------------------------------------"
+systemctl stop phpsessionclean.timer php7.4-fpm.service mariadb.service apache2.service
+echo
+echo
+echo "---------------------------------------------------------------------------------------------------"
+echo "Mount status of /dev/sda6"
+echo "---------------------------------------------------------------------------------------------------"
+findmnt /dev/sda6
+echo
+echo
+echo "---------------------------------------------------------------------------------------------------"
+echo "Unmounting /dev/sda6"
+echo "---------------------------------------------------------------------------------------------------"
+umount /dev/sda6
+echo
+echo
+echo "Exit"
